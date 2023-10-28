ac52c16c8b673f19bc087702794c7fa00165ecc7
Gabriel Basilio Souza <gb.darkaico@gmail.com>
2023-10-28 10:31:18 -0300

    remoção de print pra debug

--- src/main/java/com/SunLovers/PriseTheSun/controller/EventoController.java
+++ src/main/java/com/SunLovers/PriseTheSun/controller/EventoController.java
@@ -23,18 +23,13 @@ public class EventoController {
                 @RequestParam(required = true) String sigla,
                 @RequestParam(required = true) String descricao) {
             // Verifica se o usuário já existe (pode ser feita uma lógica de verificação aqui)
-            System.out.println("de bug 1");
             if (eventoService.eventoExiste(nome)) {
-                 System.out.println("de bug 2");
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Evento Já existe!");
                 
             } else {
-                 System.out.println("de bug 3");
                 Evento evento = new Evento(nome,sigla,descricao);
                 // Se o usuário não existe, cadastra o usuário
-                 System.out.println("de bug 4");
                 eventoService.criarEvento(evento);
-                 System.out.println("de bug 5");
                 return ResponseEntity.status(HttpStatus.CREATED).body("Evento Cadastrado com Sucesso");
             }
         }
